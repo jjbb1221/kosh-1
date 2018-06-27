cbfe987592b4156b238230eca9b443e848162f10
coolpixire <theemaraldblock@gmail.com>
2018-06-27 20:11:21 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,3 +8,10 @@ client.user.setPresence({ game: { name: '[$cmds]', type: 1 } });
 
 //THIS MUST BE THIS WAY
 client.login(process.env.BOT_TOKEN);
+
+client.on("message", () => {
+if message.startsWith(prefix + "cmds") {
+const m = await message.channel.send("Ping?");
+    m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
+}
+}):
