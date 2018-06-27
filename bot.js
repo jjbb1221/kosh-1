034bca534ef54cd28f086be702ac4d57bf0b91c2
coolpixire <theemaraldblock@gmail.com>
2018-06-27 19:50:00 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,15 +2,10 @@ const Discord = require('discord.js');
 const client = new Discord.Client()
 
 client.on('Ready', () => {
-    console.log('I am ready!');
+    console.log('[Kosh_PROCESS] [KOSH_BOT] ONLINE');
  client.user.setPresence({ game: { name: 'Serving ${client.guilds.size} servers', type: 1 } });
 });
 
-client.on('message', message => {
-    if (message.content === 'ping') {
-      message.reply('pong');
-    }
-});
 
 //THIS MUST BE THIS WAY
 client.login(process.env.BOT_TOKEN);
