3a8dd0bbe415763c7f11825219af28924480f9ca
RedstoneClaw101 <35359379+RedstoneClaw101@users.noreply.github.com>
2018-06-30 00:25:40 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -208,9 +208,11 @@ client.on("message", async message => {
   
   // Let's go with a few common example commands! Feel free to delete or change those.
   
-Discord.on('message', function (message) {
-  if (message.content === 'ping' || message.content === '!ping') {
-    return message.reply('Pong!');
+  if(command === "ping") {
+    // Calculates ping between sending a message and editing it, giving a nice round-trip latency.
+    // The second ping is an average latency between the bot and the websocket server (one-way, not round-trip)
+    const m = await message.channel.send("Ping?");
+    m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
   }
   
   if(command === "say") {
