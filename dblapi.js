3a9543c25ec75c92498c3ead39d1cb4f17688c93
RedstoneClaw101 <35359379+RedstoneClaw101@users.noreply.github.com>
2018-06-30 10:55:28 -0400

    Update dblapi.js

--- dblapi.js
+++ dblapi.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const DBL = require("dblapi.js");
-const dbl = new DBL('461547011456368640', client);
+const dbl = new DBL('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjQ2MTU0NzAxMTQ1NjM2ODY0MCIsImJvdCI6dHJ1ZSwiaWF0IjoxNTMwMzcwNDYzfQ.cO3EqFi97Sn1XY8LTkxE_PTjhYD4MrkjEIEhweRm2N8', client);
 
 // Optional events
 dbl.on('posted', () => {
