20178efdc0f97236845d4231765da80e0ec5a8fe
RedstoneClaw101 <35359379+RedstoneClaw101@users.noreply.github.com>
2018-06-30 09:44:50 -0400

    Create dblapi.js

---
+++ dblapi.js
@@ -0,0 +1,13 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const DBL = require("dblapi.js");
+const dbl = new DBL('Your discordbots.org token', client);
+
+// Optional events
+dbl.on('posted', () => {
+  console.log('Server count posted!');
+})
+
+dbl.on('error', e => {
+ console.log(`Oops! ${e}`);
+})
