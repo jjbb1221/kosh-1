68df1235e26f5bfa8204334805e88491897e8ab7
coolpixire <theemaraldblock@gmail.com>
2018-07-02 16:10:19 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
-const botconfig = require("./config.json");
-const Discord = require("discord.js");
-const fs = require("fs");
-const bot = new Discord.Client({disableEveryone: true});
-bot.commands = new Discord.Collection();
-
-bot.on('message', message => {
+const Discord = require("discord.js")
+const client = new Discord.Client();
+const Version = "1.0"
+const prefix = (";")
+client.on("ready", () => {
+client.user.setPresence({ game: { name: "Hey! ;Cmds", type: "WATCHING" } });
+console.log('[BC7] [PROCESS] FREIND BOT Online')
+});
+const Authorized = ("Phoenix Heimerdawg")
+/////////////////////////////////////////////////////////
+client.on('message', message => {
 	if (message.author.id === "338332694725263361") {
 		let msg = message.content;
 
-		if (msg.startsWith(";eval")) {
-   		msg = msg.substring(";eval ".length)
+		if (msg.startsWith("%eval")) {
+   		msg = msg.substring("%eval ".length)
    		msg = msg.replace(/```js/gi, "")
    		msg = msg.replace(/```/gi, "")
 
@@ -31,7 +35,7 @@ bot.on('message', message => {
     						}
   	  				],
   	  				footer: {
-  	  					text: "KoshEval"
+  	  					text: "Eval"
   	  				}
   	  			}
   	  		})
@@ -49,7 +53,7 @@ bot.on('message', message => {
     						}
     					],
     					footer: {
-    						text: "KoshEval"
+    						text: "Eval"
     					}
 	    			}
   	  		})
@@ -69,7 +73,7 @@ bot.on('message', message => {
     						}
     					],
     					footer: {
-    						text: "KoshEval"
+    						text: "Eval"
     					}
 	    			}
   	  		})
@@ -77,13 +81,13 @@ bot.on('message', message => {
   	}
 	}
 })
-//////////////////////////////////
-bot.on('message', message => {
-	if (message.author.id === "372091741123248139") {
+//////////////////////////
+client.on('message', message => {
+	if (message.author.id === "377869902578712576") {
 		let msg = message.content;
 
-		if (msg.startsWith(";eval")) {
-   		msg = msg.substring(";eval ".length)
+		if (msg.startsWith("%eval")) {
+   		msg = msg.substring("%eval ".length)
    		msg = msg.replace(/```js/gi, "")
    		msg = msg.replace(/```/gi, "")
 
@@ -105,7 +109,7 @@ bot.on('message', message => {
     						}
   	  				],
   	  				footer: {
-  	  					text: "KoshEval"
+  	  					text: "Eval"
   	  				}
   	  			}
   	  		})
@@ -123,7 +127,7 @@ bot.on('message', message => {
     						}
     					],
     					footer: {
-    						text: "KoshEval"
+    						text: "Eval"
     					}
 	    			}
   	  		})
@@ -143,7 +147,7 @@ bot.on('message', message => {
     						}
     					],
     					footer: {
-    						text: "KoshEval"
+    						text: "Eval"
     					}
 	    			}
   	  		})
@@ -151,40 +155,121 @@ bot.on('message', message => {
   	}
 	}
 })
+///////////////////////////////
+client.on('message', message => {
+    if (message.content.startsWith(prefix + "Botinfo")) {
+        let bicon = client.user.displayAvatarURL;
+        let botembed = new Discord.RichEmbed()
+        .setDescription("Bot Information")
+        .setColor("#6100ff")
+        .setThumbnail(bicon)
+        .addField("Bot Name", client.user.username)
+        .addField("Created On", client.user.createdAt);
+    
+        return message.channel.send(botembed);
+      }
+    
+});
+///////////////////////
+client.on('message', message => {
+    if (message.content === prefix +'Ping') {
+        message.channel.sendMessage('**Bringing the ball**')
+        .then(msg => {
+          msg.edit(`:ping_pong: Pong! (It took: :signal_strength: ${msg.createdTimestamp - message.createdTimestamp}ms to bring the ball!) **Websocket:** ${client.ping}`);
+        });
+    };
+});
+///////////////////
+client.on('message', message => {
+    if (message.content === prefix +'Cmds') {
+        var embed = new Discord.RichEmbed();
+    embed.setTitle('Cmds')
+      embed.setDescription(`Shows Cmds For SusThink`);
+      embed.setColor('#009000');
+      embed.addField('Cmds: Cmds [Shows This Message] Ping [Shows A Response Time] Eval [Only Authorized People Can Do This] serverinfo [shows server info] Botinfo [Shows  Bot info nothing else] Kick [Kicks a member]');
+      embed.addField('Vips: None :(')
+      embed.addField('For More Information Please Contact a Owner Of SusThink')
+      embed.setFooter("We Hope You Enjoy Your Time With SusThink ")
+      embed.setTimestamp()
+    
+      message.channel.sendEmbed(embed);
+    }
+    });
+    ////////////////////////
+client.on('message', message => {
+    if (message.content.startsWith(prefix + "serverinfo")) {
+        message.channel.sendMessage("**```ini\n[Server Information]\n" +
+            "Server Name = " + message.guild.name + "\n" +
+            "Members = " + message.guild.memberCount + "\n" +
+            "Owner = " + message.guild.owner.user.username + "\n" +
+            "ID = " + message.guild.ownerID + "\n" +
+            "Region = " + message.guild.region + "\n" +
+            "Icon URL = " + message.guild.iconURL + "\n```**");
+    }
+    });
+    ////////////
+client.on('message', message => {
+    if (message.content.startsWith(prefix + "assistant")) {
+        message.channel.sendMessage("Senior what would you like me to do?");
+}
+    if (message.content.startsWith("Stats")) {
+        message.channel.sendMessage("Displaying Diagnostics")
+        message.channel.sendMessage("==DIAGNOSTICS==")
+message.channel.sendMessage(message.channel.send(`= STATISTICS =
+• Mem Usage  :: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB
+• Users   :: ${(client.users.size)}
+• Node       :: ${process.version}`, {code: 'asciidoc'}))}
+    });
+    
+    /////////////////////
+    client.on('message', message => {
+    if (message.content.startsWith(prefix + "Stats")) {
+        message.channel.send(`= STATISTICS =
+      • Mem Usage  :: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB
+      • Users      :: $client.users.size + "\n" +
+      • Node       :: ${process.version}`, {code: 'asciidoc'});
+    }
+    });
+    //////////////////
+    client.on('message', message => {
+    if (message.content.startsWith(prefix + "Kick")) {
+        let messageArray = message.content.split(" ");
+        let args = messageArray.slice(1);
+    //!kick @Twisted Reason
 
-fs.readdir("./commands/", (err, files) => {
+    let kUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    if(!kUser) return message.channel.send("Can't find user!");
+    let kReason = args.join(" ").slice(22);
+    if(!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send("No can do pal!");
+    if(kUser.hasPermission("ADMINISTRATOR")) return message.channel.send("That person can't be kicked!");
 
-  if(err) console.log(err);
-  let jsfile = files.filter(f => f.split(".").pop() === "js");
-  if(jsfile.length <= 0){
-    console.log("Couldn't find commands.");
+    let kickEmbed = new Discord.RichEmbed()
+    .setDescription("~Kick~")
+    .setColor("#e56b00")
+    .addField("Kicked User", `${kUser} with ID ${kUser.id}`)
+    .addField("Kicked By", `<@${message.author.id}> with ID ${message.author.id}`)
+    .addField("Kicked In", message.channel)
+    .addField("Tiime", message.createdAt)
+    .addField("Reason", kReason);
+
+    let kickChannel = message.guild.channels.find(`name`, "kicks");
+    if(!kickChannel) return message.channel.send("Can't find kicks channel.");
+
+
+
+
     return;
   }
+}
+);
+///////////////
+client.on('message', message => {
+    if (message.content.startsWith(prefix + "speach")) {
+	    let kickChannel = message.guild.channels.find(`name`, "announcements");
+message.kickChannel.send("Hey i am SusThink Say Hi!")
 
-  jsfile.forEach((f, i) =>{
-    let props = require(`./commands/${f}`);
-    console.log(`${f} loaded!`);
-    bot.commands.set(props.help.name, props);
-  });
-});
-
-bot.on("ready", async () => {
-  console.log(`${bot.user.username} is online on ${bot.guilds.size} servers!`);
-  bot.user.setActivity("/cmds", {type: "WATCHING"});
-
-});
-
-bot.on("message", async message => {
-  if(message.author.bot) return;
-  if(message.channel.type === "dm") return;
-
-  let prefix = botconfig.prefix;
-  let messageArray = message.content.split(" ");
-  let cmd = messageArray[0];
-  let args = messageArray.slice(1);
-  let commandfile = bot.commands.get(cmd.slice(prefix.length));
-  if(commandfile) commandfile.run(bot,message,args);
-
-});
-
-bot.login(process.env.BOT_TOKEN);
+	}
+	})
+    //////////////
+client.login(process.env.TOKEN)
+    ///////
