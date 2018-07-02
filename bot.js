1bf918a23544e9d1471d58f55a859ff8b1c5d120
coolpixire <theemaraldblock@gmail.com>
2018-07-02 16:19:16 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,18 +1,19 @@
-const Discord = require('discord.js');
-const client = new Discord.Client()
-const prefix = ("/")
+const Discord = require("discord.js")
+const client = new Discord.Client();
+const Version = "1.0"
+const prefix = (";")
 client.on("ready", () => {
-    console.log("[KOSH_PROCESS] [KOSH_BOT] Online!")
-client.user.setPresence({ game: { name: '/cmds', type: 1 } });
+client.user.setPresence({ game: { name: "Hey! ;Cmds", type: "WATCHING" } });
+console.log('[BC7] [PROCESS] FREIND BOT Online')
 });
-const Authorized = ("Phoenix RedstoneClaw101")
-///////////////////////////////////////////////////////////////////////////////
+const Authorized = ("Phoenix Heimerdawg")
+/////////////////////////////////////////////////////////
 client.on('message', message => {
 	if (message.author.id === "338332694725263361") {
 		let msg = message.content;
 
-		if (msg.startsWith(";eval")) {
-   		msg = msg.substring(";eval ".length)
+		if (msg.startsWith("%eval")) {
+   		msg = msg.substring("%eval ".length)
    		msg = msg.replace(/```js/gi, "")
    		msg = msg.replace(/```/gi, "")
 
@@ -34,7 +35,7 @@ client.on('message', message => {
     						}
   	  				],
   	  				footer: {
-  	  					text: "KoshEval"
+  	  					text: "Eval"
   	  				}
   	  			}
   	  		})
@@ -52,7 +53,7 @@ client.on('message', message => {
     						}
     					],
     					footer: {
-    						text: "KoshEval"
+    						text: "Eval"
     					}
 	    			}
   	  		})
@@ -72,7 +73,7 @@ client.on('message', message => {
     						}
     					],
     					footer: {
-    						text: "KoshEval"
+    						text: "Eval"
     					}
 	    			}
   	  		})
@@ -80,13 +81,13 @@ client.on('message', message => {
   	}
 	}
 })
-//////////////////////////////////
+//////////////////////////
 client.on('message', message => {
 	if (message.author.id === "372091741123248139") {
 		let msg = message.content;
 
-		if (msg.startsWith(";eval")) {
-   		msg = msg.substring(";eval ".length)
+		if (msg.startsWith("%eval")) {
+   		msg = msg.substring("%eval ".length)
    		msg = msg.replace(/```js/gi, "")
    		msg = msg.replace(/```/gi, "")
 
@@ -108,7 +109,7 @@ client.on('message', message => {
     						}
   	  				],
   	  				footer: {
-  	  					text: "KoshEval"
+  	  					text: "Eval"
   	  				}
   	  			}
   	  		})
@@ -126,7 +127,7 @@ client.on('message', message => {
     						}
     					],
     					footer: {
-    						text: "KoshEval"
+    						text: "Eval"
     					}
 	    			}
   	  		})
@@ -146,7 +147,7 @@ client.on('message', message => {
     						}
     					],
     					footer: {
-    						text: "KoshEval"
+    						text: "Eval"
     					}
 	    			}
   	  		})
@@ -154,150 +155,121 @@ client.on('message', message => {
   	}
 	}
 })
-
-const fs = require('fs');
-client.on('message', msg => {
-  if (msg.content !== '') {
-    console.log('[CHAT]' + msg.author.tag + ' > ' + msg.content);
-  }
-})
-
-// Here we load the config.json file that contains our token and our prefix values. 
-const config = require("./config.json");
-// config.token contains the bot's token
-// config.prefix contains the message prefix.
-
-client.on("ready", () => {
-  // This event will run if the bot starts, and logs in, successfully.
-  console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
-  // Example of changing the bot's playing game to something useful. `client.user` is what the
-  // docs refer to as the "ClientUser".
-client.user.setPresence({ game: { name: 'start with /help', type: 1 } });
+///////////////////////////////
+client.on('message', message => {
+    if (message.content.startsWith(prefix + "Botinfo")) {
+        let bicon = client.user.displayAvatarURL;
+        let botembed = new Discord.RichEmbed()
+        .setDescription("Bot Information")
+        .setColor("#6100ff")
+        .setThumbnail(bicon)
+        .addField("Bot Name", client.user.username)
+        .addField("Created On", client.user.createdAt);
+    
+        return message.channel.send(botembed);
+      }
+    
 });
-
-client.on("guildCreate", guild => {
-  // This event triggers when the bot joins a guild.
-  console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
-  client.user.setActivity(`start with /help`);
+///////////////////////
+client.on('message', message => {
+    if (message.content === prefix +'Ping') {
+        message.channel.sendMessage('**Bringing the ball**')
+        .then(msg => {
+          msg.edit(`:ping_pong: Pong! (It took: :signal_strength: ${msg.createdTimestamp - message.createdTimestamp}ms to bring the ball!) **Websocket:** ${client.ping}`);
+        });
+    };
 });
-
-client.on("guildDelete", guild => {
-  // this event triggers when the bot is removed from a guild.
-  console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
-  client.user.setActivity(`start with /help`);
-});
-
-
-client.on("message", async message => {
-  // This event will run on every single message received, from any channel or DM.
-  
-  // It's good practice to ignore other bots. This also makes your bot ignore itself
-  // and not get into a spam loop (we call that "botception").
-  if(message.author.bot) return;
-  
-  // Also good practice to ignore any message that does not start with our prefix, 
-  // which is set in the configuration file.
-  if(message.content.indexOf(config.prefix) !== 0) return;
-  
-  // Here we separate our "command" name, and our "arguments" for the command. 
-  // e.g. if we have the message "+say Is this the real life?" , we'll get the following:
-  // command = say
-  // args = ["Is", "this", "the", "real", "life?"]
-  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
-  const command = args.shift().toLowerCase();
-  
-  // Let's go with a few common example commands! Feel free to delete or change those.
-  
-  if(command === "ping") {
-    // Calculates ping between sending a message and editing it, giving a nice round-trip latency.
-    // The second ping is an average latency between the bot and the websocket server (one-way, not round-trip)
-    const m = await message.channel.send("Ping?");
-    m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
-  }
-	 
-  if(command === "say") {
-    // makes the bot say something and delete the message. As an example, it's open to anyone to use. 
-    // To get the "message" itself we join the `args` back into a string with spaces: 
-    const sayMessage = args.join(" ");
-    // Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley thing.
-    message.delete().catch(O_o=>{}); 
-    // And we get the bot to say the thing: 
-    message.channel.send(sayMessage);
-  }
-  
-  if(command === "kick") {
-    // This command must be limited to mods and admins. In this example we just hardcode the role names.
-    // Please read on Array.some() to understand this bit: 
-    // https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/Array/some?
-    if(!message.member.roles.some(r=>["Admin"].includes(r.name)) )
-      return message.reply("Sorry, you don't have permissions to use this!");
+///////////////////
+client.on('message', message => {
+    if (message.content === prefix +'Cmds') {
+        var embed = new Discord.RichEmbed();
+    embed.setTitle('Cmds')
+      embed.setDescription(`Shows Cmds For Kosh`);
+      embed.setColor('#009000');
+      embed.addField('Cmds: Cmds [Shows This Message] Ping [Shows A Response Time] Eval [Only Authorized People Can Do This] serverinfo [shows server info] Botinfo [Shows  Bot info nothing else] Kick [Kicks a member]');
+      embed.addField('Vips: None :(')
+      embed.addField('For More Information Please Contact a Owner Of Kosh')
+      embed.setFooter("We Hope You Enjoy Your Time With Kosh")
+      embed.setTimestamp()
     
-    // Let's first check if we have a member and if we can kick them!
-    // message.mentions.members is a collection of people that have been mentioned, as GuildMembers.
-    // We can also support getting the member by ID, which would be args[0]
-    let member = message.mentions.members.first() || message.guild.members.get(args[0]);
-    if(!member)
-      return message.reply("Please mention a valid member of this server");
-    if(!member.kickable) 
-      return message.reply("I cannot kick this user! Do they have a higher role? Do I have kick permissions?");
-    
-    // slice(1) removes the first part, which here should be the user mention or ID
-    // join(' ') takes all the various parts to make it a single string.
-    let reason = args.slice(1).join(' ');
-    if(!reason) reason = "No reason provided";
-    
-    // Now, time for a swift kick in the nuts!
-    await member.kick(reason)
-      .catch(error => message.reply(`Sorry ${message.author} I couldn't kick because of : ${error}`));
-    message.reply(`${member.user.tag} has been kicked by ${message.author.tag} because: ${reason}`);
-
-  }
-	
-  if(command === "ban") {
-    // Most of this command is identical to kick, except that here we'll only let admins do it.
-    // In the real world mods could ban too, but this is just an example, right? ;)
-    if(!message.member.roles.some(r=>["Admin"].includes(r.name)) )
-      return message.reply("Sorry, you don't have permissions to use this!");
-    
-    let member = message.mentions.members.first();
-    if(!member)
-      return message.reply("Please mention a valid member of this server");
-    if(!member.bannable) 
-      return message.reply("I cannot ban this user! Do they have a higher role? Do I have ban permissions?");
-
-    let reason = args.slice(1).join(' ');
-    if(!reason) reason = "No reason provided";
-    
-    await member.ban(reason)
-      .catch(error => message.reply(`Sorry ${message.author} I couldn't ban because of : ${error}`));
-    message.reply(`${member.user.tag} has been banned by ${message.author.tag} because: ${reason}`);
-  
-  }
-
-    if(command === "purge") {
-    // This command removes all messages from all users in the channel, up to 100.
-    
-    // get the delete count, as an actual number.
-    const deleteCount = parseInt(args[0], 10);
-    
-    // Ooooh nice, combined conditions. <3
-    if(!deleteCount || deleteCount < 2 || deleteCount > 100)
-      return message.reply("Please provide a number between 2 and 100 for the number of messages to delete");
-    
-    // So we get our messages, and delete them. Simple enough, right?
-    const fetched = await message.channel.fetchMessages({limit: deleteCount});
-    message.channel.bulkDelete(fetched)
-      .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
-  }
-});
-		    
-
-client.on('message', msg => {
-if(msg.startswith == "Api") {
-  message.channel.send("Phoenix and Hasan's API , Phoenix's Asistant  , Blaze")
+      message.channel.sendEmbed(embed);
+    }
+    });
+    ////////////////////////
+client.on('message', message => {
+    if (message.content.startsWith(prefix + "serverinfo")) {
+        message.channel.sendMessage("**```ini\n[Server Information]\n" +
+            "Server Name = " + message.guild.name + "\n" +
+            "Members = " + message.guild.memberCount + "\n" +
+            "Owner = " + message.guild.owner.user.username + "\n" +
+            "ID = " + message.guild.ownerID + "\n" +
+            "Region = " + message.guild.region + "\n" +
+            "Icon URL = " + message.guild.iconURL + "\n```**");
+    }
+    });
+    ////////////
+client.on('message', message => {
+    if (message.content.startsWith(prefix + "assistant")) {
+        message.channel.sendMessage("Senior what would you like me to do?");
 }
-});
+    if (message.content.startsWith("Stats")) {
+        message.channel.sendMessage("Displaying Diagnostics")
+        message.channel.sendMessage("==DIAGNOSTICS==")
+message.channel.sendMessage(message.channel.send(`= STATISTICS =
+• Mem Usage  :: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB
+• Users   :: ${(client.users.size)}
+• Node       :: ${process.version}`, {code: 'asciidoc'}))}
+    });
+    
+    /////////////////////
+    client.on('message', message => {
+    if (message.content.startsWith(prefix + "Stats")) {
+        message.channel.send(`= STATISTICS =
+      • Mem Usage  :: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB
+      • Users      :: $client.users.size + "\n" +
+      • Node       :: ${process.version}`, {code: 'asciidoc'});
+    }
+    });
+    //////////////////
+    client.on('message', message => {
+    if (message.content.startsWith(prefix + "Kick")) {
+        let messageArray = message.content.split(" ");
+        let args = messageArray.slice(1);
+    //!kick @Twisted Reason
+
+    let kUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    if(!kUser) return message.channel.send("Can't find user!");
+    let kReason = args.join(" ").slice(22);
+    if(!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send("No can do pal!");
+    if(kUser.hasPermission("ADMINISTRATOR")) return message.channel.send("That person can't be kicked!");
+
+    let kickEmbed = new Discord.RichEmbed()
+    .setDescription("~Kick~")
+    .setColor("#e56b00")
+    .addField("Kicked User", `${kUser} with ID ${kUser.id}`)
+    .addField("Kicked By", `<@${message.author.id}> with ID ${message.author.id}`)
+    .addField("Kicked In", message.channel)
+    .addField("Tiime", message.createdAt)
+    .addField("Reason", kReason);
+
+    let kickChannel = message.guild.channels.find(`name`, "kicks");
+    if(!kickChannel) return message.channel.send("Can't find kicks channel.");
 
 
 
-client.login(process.env.BOT_TOKEN);
+
+    return;
+  }
+}
+);
+///////////////
+client.on('message', message => {
+    if (message.content.startsWith(prefix + "speach")) {
+	    let kickChannel = message.guild.channels.find(`name`, "announcements");
+message.kickChannel.send("Hey i am Kosh Say Hi!")
+
+	}
+	})
+    //////////////
+client.login(process.env.TOKEN)
+    ///////
